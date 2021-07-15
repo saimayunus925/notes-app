25333e1db47e87db1b62d1c23b5c49914c8b2f5a
Saima Yunus <saimayunus925@gmail.com>
2021-07-15 16:11:57 -0700

    made run.py code

---
+++ run.py
@@ -0,0 +1,7 @@
+
+from .notes_app import notes_app # importing our app object
+
+if __name__ == "__main__": 
+    # if the current module's name == the main program's name, i.e. if the current module is the main program
+    # then the app will run in debug mode (server updates upon code changes)
+    notes_app.run(debug=True)
