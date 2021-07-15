08b55715ebda3e17bb8ee15eb7debf855e74831f
Saima Yunus <saimayunus925@gmail.com>
2021-07-15 16:38:47 -0700

    added basic login form to forms.py

--- notes_app/forms.py
+++ notes_app/forms.py
@@ -12,8 +12,14 @@ class SignUp(Form):
     username = StringField('Username', [validators.DataRequired()]) # account username, can't be empty
     email = StringField('Email', [validators.DataRequired(), validators.Email()]) # account email, can't be empty and must be an email address
     password = PasswordField('Password', [
-        validators.Length(min=12),
+        validators.Length(min=12, message="ERROR - password must be at least 12 characters long."),
         validators.DataRequired('confirm_password', message="ERROR - new password and confirmed password don't match")
-    ]) # account password, can't be empty and must be at least 12 chars long, also must match 'confirm password' fi
+    ]) # account password, can't be empty and must be at least 12 chars long, also must match 'confirm password' field
     confirm_password = PasswordField('Confirm Password', [validators.DataRequired()]) # this field helps us confirm our password by typing it again, can't be empty ofc
     submit_button = SubmitField('Sign Up') # the 'submit' button for the signup form
+
+# 'login' form
+class SignIn(Form):
+    username = StringField('Username', [validators.DataRequired()]) # account username, can't be empty
+    password = PasswordField('Password', [validators.Length(min=12), validators.DataRequired()]) # account password, can't be empty and must be at least 12 chars long
+    submit_button = SubmitField('Sign In') # the 'submit' button for the sign-in form
