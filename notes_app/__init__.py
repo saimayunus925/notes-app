0d35b7765f1dec0b9ee238aff65bd5bb57ad979a
Saima Yunus <saimayunus925@gmail.com>
2021-07-19 12:32:02 -0700

    fixed config of DB

--- notes_app/__init__.py
+++ notes_app/__init__.py
@@ -3,12 +3,18 @@ from flask import Flask # importing Flask obj from "flask" module
 from flask_pymongo import PyMongo # importing PyMongo() obj to connect/interface with our MongoDB database
 from .forms import AddNote # testing if we can import from 'forms.py' 
 
+import os # Python's module for interacting with the OS
+from dotenv import load_dotenv # this func finds a .env file in the project dir; if it finds one, 
+# it loads the environment variables in the file and helps project access them
+load_dotenv() # loading our env variables 
+
 notes_app = Flask(__name__) # initializing our app obj with the current module name
-notes_app.config["MONGO_URI"] = "" # configuring the DB's location here
+notes_app.config["MONGO_URI"] = os.getenv("MONGO_URI") # configuring the DB's location here
 notesDB = PyMongo(notes_app) # initializing PyMongo() obj for use with our app obj
 
 ''' ROUTES ARE BELOW FOR NOW '''
 
+# index route
 @notes_app.route('/')
 @notes_app.route('/index')
 def index():
