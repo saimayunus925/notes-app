869a81835b61a79e127748c54602d0bfc095adaf
Saima Yunus <saimayunus925@gmail.com>
2021-07-24 18:49:29 -0700

    created basic 'User' data model

--- notes_app/models.py
+++ notes_app/models.py
@@ -5,6 +5,16 @@
 from notes_app import notesDB # importing our SQLAlchemy DB obj with which to interface with our SQLite database
 from datetime import datetime # use this to get date/time objs
 
+# 'User' class/model/table
+class User(notesDB.Model):
+    id = notesDB.Column(notesDB.Integer, primary_key=True) # unique ID/primary key for each 'user' record in the 'users' table
+    username = notesDB.Column(notesDB.String(180), unique=True, nullable=False) # user username, 180 chars max, MUST be unique, CANNOT be empty
+    email = notesDB.Column(notesDB.String(220), unique=True, nullable=False) # user email, 220 chars max, MUST be unique, CANNOT be empty
+
+    def __repr__(self):
+        # this is how User objs will be printed
+        return f"<User: {self.username}, {self.email}>"
+
 # 'Note' class/model/table
 class Note(notesDB.Model):
     id = notesDB.Column(notesDB.Integer, primary_key=True) # unique ID/primary key for each 'note' record in the 'notes' table
